4470ae1f5541c6017782cbc59c698ecc9960ecf2
Huy Nguyen <willber.es@gmail.com>
2017-04-16 15:52:29 -0700

    Update Readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-An HTML/CSS/Javascript template showing a list of your favorite movies generated using Python.
+An HTML/CSS/Javascript template showing a list of your favorite movies generated from Python.
 
 ## How to run
 
@@ -11,9 +11,9 @@ An HTML/CSS/Javascript template showing a list of your favorite movies generated
 
 ## Modifications made to the original code
 
-* Change the page title
+* New page title
 * Shows movie's summary on mouse hover
 * Blur movie's poster on mouse hover
-* New background downloaded from Subtle Patterns
+* New background downloaded from [Subtle Pattern](https://www.toptal.com/designers/subtlepatterns/?s=swirl_pattern)
 * New font from Google Fonts
 * Increased Youtube window size
--- movies.py
+++ movies.py
@@ -2,17 +2,17 @@ import best_tomatoes
 import media
 
 logan = media.Movie("Logan",
-                        "The Wolverine meets a young mutant girl who is very much like him.",
-                        "http://www.beliefnet.com/columnists/moviemom/files/2017/03/logan.jpg",
-                        "https://www.youtube.com/watch?v=RH3OxVFvTeg")
+                    "The Wolverine meets a young mutant girl who is very much like him.",
+                    "http://www.beliefnet.com/columnists/moviemom/files/2017/03/logan.jpg",
+                    "https://www.youtube.com/watch?v=RH3OxVFvTeg")
 
 jungle_book = media.Movie("The Jungle Book",
-                     "A story about a boy who grew up in the jungle",
-                     "http://t1.gstatic.com/images?q=tbn:ANd9GcQgNaB5wtt0G7_mTFVygkFtmjWut_Z0QSz2GzDsHeiZDHno4fjh",
-                     "https://www.youtube.com/watch?v=5mkm22yO-bs")
+                          "A story about a boy who grew up in the jungle",
+                          "http://t1.gstatic.com/images?q=tbn:ANd9GcQgNaB5wtt0G7_mTFVygkFtmjWut_Z0QSz2GzDsHeiZDHno4fjh",
+                          "https://www.youtube.com/watch?v=5mkm22yO-bs")
 
 the_great_wall = media.Movie("The Great Wall",
-                             "Two foreigners found them self stuck in a mystical war at the Great Wall of China",
+                             "Two foreigners found them self stuck in a mystical war at the Great Wall of China.",
                              "https://assets.voxcinemas.com/posters/P_HO00004241.jpg",
                              "https://www.youtube.com/watch?v=avF6GHyyk5c")
 
@@ -27,7 +27,7 @@ scarface = media.Movie("Scarface",
                        "https://www.youtube.com/watch?v=7pQQHnqBa2E")
 
 casino_royal = media.Movie("Casino Royal",
-                           "James Bond on another mission",
+                           "James Bond on another mission.",
                            "http://www.gstatic.com/tv/thumb/movieposters/159167/p159167_p_v8_aa.jpg",
                            "https://www.youtube.com/watch?v=fl5WHj0bZ2Q")
 
